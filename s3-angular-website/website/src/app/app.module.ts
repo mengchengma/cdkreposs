98557045fc645a10aa437a2b6491c322bf769cb7
EC2 Default User <ec2-user@ip-172-31-69-45.ec2.internal>
2021-08-04 16:07:20 +0000

    components done

--- s3-angular-website/website/src/app/app.module.ts
+++ s3-angular-website/website/src/app/app.module.ts
@@ -4,9 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  AccordionModule,
+  AlertModule,        // Foundation Callouts
+  ButtonsModule,
+  CarouselModule,     // Foundation Orbit
+  CollapseModule,
+  BsDatepickerModule,
+  BsDropdownModule,   // Foundation Dropdown Menus and Dropdown Panes
+  ModalModule,        // Foundation Reveal
+  OffcanvasModule,
+  PaginationModule,
+  ProgressbarModule,
+  RatingModule,
+  SortableModule,
+  TabsModule,
+  TimepickerModule,
+  TooltipModule,
+  TypeaheadModule,
+} from 'ngx-foundation';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HeroComponent } from './hero/hero.component';
+import { FeaturesComponent } from './features/features.component';
+import { AboutComponent } from './about/about.component';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HeroComponent,
+    FeaturesComponent,
+    AboutComponent,
+    FeedbackFormComponent
   ],
   imports: [
     BrowserModule,
